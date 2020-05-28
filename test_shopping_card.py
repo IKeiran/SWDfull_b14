55cf260dc2ef8edae2095a43107ec665362cdd00
Mykhailo Siniakov <keiran@Mykhailos-MacBook-Pro.local>
2020-05-28 09:24:28 +0300

    fix product locator

--- test_shopping_card.py
+++ test_shopping_card.py
@@ -16,7 +16,7 @@ def setup_environment():
 def test_check_sticker(setup_environment):
     base_url = 'http://192.168.64.2/litecart/'
     driver.get(base_url)
-    xpath = "//li[@class='product']"
+    xpath = "//li[@class='product column shadow hover-light']"
     goods_items = driver.find_elements_by_xpath(xpath)
     for item in goods_items:
         sticker_count = item.find_elements_by_class_name('sticker')
